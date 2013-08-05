a999837cd78791f7947bca2c0ef62146b8b2f49c
tathata <ryan.lindgren@gmail.com>
2013-08-05 12:10:47 -0400

    adds Chinese language

--- app/assets/javascripts/utils.js
+++ app/assets/javascripts/utils.js
@@ -1,27 +1,24 @@
-// kill the address bar in mobile Safari (iOS < 7.0)
-window.addEventListener("load",function() {
-	setTimeout(function(){
-		window.scrollTo(0, 1);
-	}, 0);
-});
 
 // Utility functions
 
 (function ($) {
 
+	function getLangURI (loc) {
+		var check = window.location.pathname.match(/cn/i);
+		if (check) {
+			return loc.href.split('/cn').join('')
+		} else {
+			return loc.origin + '/cn' + loc.pathname
+		}
+	}
+
 	// force the language button to navigate to the same page
 	// but in the opposite language
-	(function () {
-		$('#lang').click(function (ev) {
-			var loc = window.location;
-			ev.preventDefault();
-			if (loc.pathname.match(/\/cn\//gi)) {
-				loc.href = loc.origin + loc.pathname.slice(3);
-			} else {
-				loc.href = loc.origin + "/cn" + loc.pathname;
-		  }
-		});
-	}());	
+	$('#lang').click(function (ev) {
+		var loc = window.location;
+		ev.preventDefault();
+		loc.href = getLangURI(loc);
+	});	
 
 }(jQuery))
 
