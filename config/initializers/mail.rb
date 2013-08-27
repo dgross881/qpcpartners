06d7950b32303d1c22df7e792148c0be22596ae7
tathata <ryan.lindgren@gmail.com>
2013-08-27 12:51:29 -0400

    set up mail to relay through godaddy

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -3,8 +3,8 @@ Qpcpartners::Application.configure do
 	config.action_mailer.smtp_settings = {
 	  enable_starttls_auto: true,
 	  address: 'smtpout.secureserver.net',
-	  port: 587,
-	  domain: 'email.secureserver.com',
+	  port: 80,
+	  domain: 'www.qpcpartners.com',
 	  authentication: 'plain',
 	  user_name: 'rlindgren@qpcpartners.com',
 	  password: IO.read(File.join(Rails.root, '/config/mailpass.txt'))
