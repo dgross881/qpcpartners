6c4059b781a2125c7dae44fe17a4a21dc99e785d
tathata <ryan.lindgren@gmail.com>
2013-08-28 16:47:08 -0400

    remove redis

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,15 +1,17 @@
 #encoding: utf-8
 
 class ContactMailer < ActionMailer::Base
-	default :to => @contact.email, :from => "QPC Partners <no-reply@qpcpartners.com>"
+	default :from => "QPC Partners <no-reply@qpcpartners.com>"
 
   def thank_you_email(contact)
   	@contact = contact
-  	mail(:subject => "Thank you for contacting QPC Partners")
+  	mail(:to => @contact.email,
+          :subject => "Thank you for contacting QPC Partners")
   end
 
   def thank_you_email_cn(contact)
   	@contact = contact
-  	mail(:subject => "QPC Partners - 您发的信息已收到")
+  	mail(:to => @contact.email,
+          :subject => "QPC Partners - 您发的信息已收到")
   end
 end
