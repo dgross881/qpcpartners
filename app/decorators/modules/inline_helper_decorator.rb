a53e3398817f281889b45327530253fb244d0249
dgross881 <dgross881@gmail.com>
2017-03-03 10:51:22 -0600

    removed pry

--- app/decorators/modules/inline_helper_decorator.rb
+++ app/decorators/modules/inline_helper_decorator.rb
@@ -1,7 +1,6 @@
 InlineHelper.module_eval do
   def phrase(key, options = {}, *args)
-    key = key.to_s 
-    binding.pry
+    key = key.to_s
     if can_edit_phrases?
       @record = PhrasingPhrase.where(key: key, locale: I18n.locale.to_s).first || PhrasingPhrase.create_phrase(key)
       inline(@record, :value, options)
