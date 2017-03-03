3e520b6a4e4e14373c442c6f15aad50e31ba3345
dgross881 <dgross881@gmail.com>
2017-03-03 10:49:46 -0600

    customized phrasing inline helper

---
+++ app/decorators/modules/inline_helper_decorator.rb
@@ -0,0 +1,12 @@
+InlineHelper.module_eval do
+  def phrase(key, options = {}, *args)
+    key = key.to_s 
+    binding.pry
+    if can_edit_phrases?
+      @record = PhrasingPhrase.where(key: key, locale: I18n.locale.to_s).first || PhrasingPhrase.create_phrase(key)
+      inline(@record, :value, options)
+    else
+      t(key, *args).html_safe
+    end
+  end
+end
